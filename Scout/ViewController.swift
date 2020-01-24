b61ed63a743b1196964085c82f7accbc65ab6407
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2020-01-25 00:15:35 +0300

    Initial Commit

---
+++ Scout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scout
+//
+//  Created by Vedat Bilaloğlu on 25.01.2020.
+//  Copyright © 2020 Vedat Bilaloglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
