39d184dd126c70a46418a0ffa0d8e29571ce9802
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2020-01-25 21:00:51 +0300

    first commit

---
+++ Scout/Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  Scout
+//
+//  Created by Vedat Bilaloğlu on 25.01.2020.
+//  Copyright © 2020 Vedat Bilaloglu. All rights reserved.
+//
+
+import Foundation
---
+++ Scout/scoutVC.swift
@@ -0,0 +1,30 @@
+//
+//  scoutVC.swift
+//  Scout
+//
+//  Created by Vedat Bilaloğlu on 25.01.2020.
+//  Copyright © 2020 Vedat Bilaloglu. All rights reserved.
+//
+
+import UIKit
+
+class scoutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
